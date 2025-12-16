ae77d70a6924f439903f6aa2765831ae85adbbd2
Soutick Samanta <soutick1505@gmail.com>
2025-12-16 16:39:35 +0530

    add button click sound

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { 
   let string = '';
   const inputField = document.querySelector('input');
   const buttons = document.querySelectorAll('.btn');
 
+  const clickSound = new Audio('Sound/click-sound.mp3'); 
+
   buttons.forEach(button => {
     button.addEventListener('click', (e) => {
+
+      clickSound.currentTime = 0; 
+      clickSound.play();
+
       const buttonValue = e.target.innerHTML;
 
       if (buttonValue === '=') {
